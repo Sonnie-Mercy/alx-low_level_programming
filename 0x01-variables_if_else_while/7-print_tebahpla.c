3a1bc7b90f220af39e0dbed8e6c47452fe2a417e
Mercy Ndirangu <ndirangumercy4@gmail.com>
2022-10-16 12:04:23 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase letters in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'z'; la >= 'a' la--)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
