bf1a7c7eaaa4342b53aa5a3c595af1c075120205
Mercy Ndirangu <ndirangumercy4@gmail.com>
2022-10-18 02:33:47 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Prints _putchar
- *
+ * is written by Sonnie-Mercy
  * Return: Always 0
  */
+
 int main(void)
 {
-	char *dh = "_putchar";
-
-	while (*dh)
-	{
-		_putchar(*dh);
-		dh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
