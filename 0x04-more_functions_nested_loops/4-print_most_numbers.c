dcbeb80603c7fa92bc8741f3b8e17fd529d91412
Mercy Ndirangu <ndirangumercy4@gmail.com>
2022-10-22 07:04:35 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers and 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void);
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+		a++;
+	}
+	_putchar('\n');
+}
