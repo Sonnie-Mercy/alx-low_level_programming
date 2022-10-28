696055f7d972d6c321d2e24fefd554bdd9db10cc
Mercy Ndirangu <ndirangumercy4@gmail.com>
2022-10-28 00:13:59 -0700

    Task 0 -change to dest

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,17 +2,17 @@
 /**
  * _strcat - concatenates two strings
  * @src: source string
- * @end: string to concatenate to
+ * @dest: string to concatenate to
  * Return: pointer to resulting string
  */
-char *_strcat(char *end, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0;
-	int end_len = 0;
+	int dest_len = 0;
 
-	while (end[index++])
-		end_len++;
+	while (dest[index++])
+		dest_len++;
 	for (index = 0; src[index]; index++)
-		end[end_len++] = src[index];
-	return (end);
+		dest[dest_len++] = src[index];
+	return (dest);
 }
