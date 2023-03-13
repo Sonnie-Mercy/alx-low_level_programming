2512760772594590f2c2df90c4cc03d63979f0d1
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-13 07:27:37 -0700

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 		for (j = 0; j <= 9; j++)
 			if (i < j)
 			{
-				putchar ('0' + i);
-				putchar ('0' + j);
+				putchar (i);
+				putchar (j);
 				if ((i != '8') || (j != '9'))
 				{
-					putchar (44);
+					putchar (',');
 					putchar (' ');
 				}
 			}
