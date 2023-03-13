94b680d2ea9fdc55e6edb7377f09a7b0b6414ae8
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-13 08:05:41 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,12 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-		if (n != 9)
-		{
-			putchar(n + '0');
-			putchar(',');
-			putchar(' ');
-		}
-	else
-	{
 		putchar(n + '0');
+	if (n != 9)
+	{
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
