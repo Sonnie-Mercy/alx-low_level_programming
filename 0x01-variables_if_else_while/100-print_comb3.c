c17e11f6a17a89b7af62d172e8ca4ff204277044
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-13 06:33:06 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - program that prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+			if (i != j)
+			{
+				putchar ('0' + i);
+				putchar ('0' + j);
+				putchar (44);
+				putchar (' ');
+			}
+	}
+	return (0);
+}
