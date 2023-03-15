445207131477ad5b533965fcd040b3201fc859d4
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-15 11:36:44 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 
 			if (product < 10 && mult2 != 0)
 			{
-				_putchar(',')
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
@@ -27,8 +27,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((product / 10 + '0');
-				_putchar((product % 10 + '0');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 	_putchar('\n');
