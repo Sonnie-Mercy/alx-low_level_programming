dd258ab4e3a53da7e60ecbaeac095b654419feaf
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-22 09:48:55 -0700

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""Creating an island described in grid"""
+"""making a grid with an island"""
 
 
 def island_perimeter(grid):
     """
-    returns the perimeter of the island described in grid
-    @grid: a list of lists of ints
+    Returns the perimeter of the island described in grid.
     """
+    m, n = len(grid), len(grid[0])
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+
+    for i in range(m):
+        for j in range(n):
             if grid[i][j] == 1:
                 perimeter += 4
-                if i > 0:
-                    if grid[i - 1][j] != 0:
-                        perimeter -= 1
-                    if i < len(grid) - 1:
-                        if grid[i + 1][j] != 0:
-                            perimeter -= 1
-                if j > 0:
-                    if grid[j - 1][i] != 0:
-                        perimeter -= 1
-                    if j < len(grid[0]) - 1:
-                        if grid[i][j + 1] != 0:
-                            perimeter -= 1
+
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+
     return perimeter
