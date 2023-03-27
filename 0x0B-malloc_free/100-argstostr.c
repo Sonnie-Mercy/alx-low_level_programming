c24623667888dc34ffc19abf1baff8ae323ad875
Mercy Ndirangu <ndirangumercy4@gmail.com>
2023-03-27 11:18:55 -0700

    first advanced task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * argstostr - concatanates all the arguments of the program
+ * @ac: integer number of array elements
+ * @av: array of character strings
+ * Return: NULL if av or ac is NULL else pointer to new string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			len++;
+
+	str = malloc(sizeof(char) * (ac + len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k++] = '\n';
+	}
+	str[k] = '\n';
+	return (str);
+}
