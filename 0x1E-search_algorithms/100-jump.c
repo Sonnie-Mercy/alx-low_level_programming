bb62f69c49ff7429fd7962b0cc1581933cf1b7c0
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-12 02:36:05 -0700

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 	previous = 0;
 	current = jump;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 
 	while (current < size && array[current] < value)
