b8221ac998269ca09ba9cdc0a54a09c438b581d3
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-14 06:52:12 -0700

    task advanced

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,9 +9,17 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n > -10)
+		{
+			_putchar('0' - n);
+			return;
+		}
+		print_number(n / -10);
 	}
-	if (n / 10)
+
+	else if (n >= 10)
+	{
 		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	}
+	_putchar((n % 10 >= 0 ? n % 10 : -n % 10) + '0');
 }
